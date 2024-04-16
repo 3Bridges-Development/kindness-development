dc253f6aebc66a9fc27880b1433a0a41f54b1de5
nkleinmann <nekleinmann@gmail.com>
2024-04-15 20:48:03 -0400

    Add starter for other components

---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import kdclogo from "../Assets/kdclogo.png";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function AboutUs() {
+    return (
+    <Container className="aboutus">
+        <Row>
+            <Col id="aboutus">
+                About Us
+            </Col>
+        </Row>
+        <Row>
+            <Col className="image" md="6">
+                <img src={kdclogo} width="25%" alt="circle with kindness development center inc" className="mainLogo"/>
+                {/* <span className="title">Kindness Development Center Inc.</span> */}
+            </Col>
+            <Col className="text" md="6">
+                <Row>
+                    <span>To Create a Safe Space, for youth, young adults, and adults with Disabilities. To ensure they have an opportunity to reach their highest levels of skill, purpose, dignity, and respect. Through commitment, advocacy, family, community, and partnerships. </span>
+                </Row>
+            </Col>
+        </Row>
+        <Row>
+            Optional Gallery component for images?
+        </Row>
+    </Container>
+    )
+}
+
+export default AboutUs;
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "../Components/Header";
-import kdclogo from "../Assets/kdclogo.png";
+// import kdclogo from "../Assets/kdclogo.png";
 import Mission from "../Components/Mission";
 import MainBlurb from "../Components/MainBlurb";
+import AboutUs from "../Components/AboutUs";
+import Center from "../Components/Center";
 
 function Home() {
     return (
@@ -11,6 +13,8 @@ function Home() {
             <MainBlurb />
             {/* <img src={kdclogo} width="10%" alt="circle with kindness development center inc" className="mainLogo"/> */}
             <Mission />
+            <Center />
+            <AboutUs />
         </div>
     )
 }
