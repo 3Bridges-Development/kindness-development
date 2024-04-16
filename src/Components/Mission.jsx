fe62c22255b7db7816761da798887737e6ff9a28
nkleinmann <nekleinmann@gmail.com>
2024-04-15 20:34:39 -0400

    Add the mission and placeholder image

---
+++ src/Components/Mission.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import kdclogo from "../Assets/kdclogo.png";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Mission() {
+    return (
+    <Container className="mission">
+        <Row>
+            <Col id="mission">
+                Mission
+            </Col>
+        </Row>
+        <Row>
+            <Col className="image" md="6">
+                <img src={kdclogo} width="25%" alt="circle with kindness development center inc" className="mainLogo"/>
+                {/* <span className="title">Kindness Development Center Inc.</span> */}
+            </Col>
+            <Col className="text" md="6">
+                <Row>
+                    <span>To Create a Safe Space, for youth, young adults, and adults with Disabilities. To ensure they have an opportunity to reach their highest levels of skill, purpose, dignity, and respect. Through commitment, advocacy, family, community, and partnerships. </span>
+                </Row>
+            </Col>
+
+        </Row>
+    </Container>
+    )
+}
+
+export default Mission;
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../Components/Header";
 import kdclogo from "../Assets/kdclogo.png";
+import Mission from "../Components/Mission";
+import MainBlurb from "../Components/MainBlurb";
 
 function Home() {
     return (
         <div className="home">
             <Header />
-            <img src={kdclogo} width="10%" alt="circle with kindness development center inc" className="mainLogo"/>
+            <MainBlurb />
+            {/* <img src={kdclogo} width="10%" alt="circle with kindness development center inc" className="mainLogo"/> */}
+            <Mission />
         </div>
     )
 }
