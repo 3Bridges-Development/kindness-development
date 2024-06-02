f8aea41cbdb1a7bfb81c154ee281ce4ebb30dbee
nkleinmann <nekleinmann@gmail.com>
2024-06-02 18:20:07 -0400

    Most of Services

--- src/App.js
+++ src/App.js
@@ -32,7 +32,10 @@ query {
     items{
       programsAndEnrollment
     }
-  }
+  },
+  academyFutureInnovationsCollection{items{
+    futureInnovations
+  }}
   }
 `
 
--- src/Components/AcademyInfo.jsx
+++ src/Components/AcademyInfo.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import plugIcon from "../Assets/plug.png";
+import { useOutletContext } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
 
 function AcademyInfo() {
+    const data = useOutletContext();
+    let i=0;
 
     return (
         <section className="academyInfo">
             <Row className="justify-content-center" id="academy" style={{paddingTop: "200px"}}>
                 <Col className="firstProgramText" md="10">
-                    <Row className="justify-content-center">
-                        <Col>
-                            <p className="programHeader">Broadcasting Training Program</p>
-                            <p className="programInfo">Will provide the individual with hands on learning in all aspects of broadcasting. While developing social, mental and physical skills.</p>
-                        </Col>
-                    </Row>
                     <Row className="justify-content-center" style={{paddingBottom: "50px", paddingTop: "50px"}}>
                         <Col md="6" className="futureText" id="futureinnovations">
-                            <p className="programHeader">Future Innovations</p>
-                            <p className="programInfo">Music Therapy Studio</p>
-                            <p className="programInfo">Food Pantry</p>
-                            <p className="programInfo">Counseling</p>
+                        {data.academyFutureInnovationsCollection.items ? (
+                            data.academyFutureInnovationsCollection.items.map(futureProgram => (
+                                <ReactMarkdown key={futureProgram.futureInnovations}>
+                                    {futureProgram.futureInnovations}
+                                </ReactMarkdown>
+                            ))
+                        ) : ""}
                         </Col>
                         <Col md="3" className="imageCol">
-                            <img width="100%" src={plugIcon} alt="plug" className="bioImage" />
-                            <p>image of thinking character or icon</p>
+                            <img width="100%" src={plugIcon} alt="plug" className="futureInnovationsImage" />
+                            <p>Future Innovations</p>
                         </Col>
                     </Row>
                 </Col>
