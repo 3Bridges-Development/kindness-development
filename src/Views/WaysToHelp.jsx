571a6ee69baf60a96c9c5131b83b7e4f311359cb
nkleinmann <nekleinmann@gmail.com>
2024-08-04 17:37:37 -0400

    add header

--- src/Views/WaysToHelp.jsx
+++ src/Views/WaysToHelp.jsx
@@ -30,15 +30,20 @@ function WaysToHelp() {
         <>
             <Row className="theWaysToHelp" id="howToHelp">
                 <Col md="12 justify-content-center">
-                <script async
-                src="https://js.stripe.com/v3/buy-button.js">
-                </script>
+                    <h1>Donate Now</h1>
+                </Col>
+            </Row>
+            <Row className="theWaysToHelp">
+                <Col md="12 justify-content-center">
+                    <script async
+                    src="https://js.stripe.com/v3/buy-button.js">
+                    </script>
 
-                <stripe-buy-button
-                buy-button-id="buy_btn_1Pjpm2Roha3LTV1IHrYUvfuu"
-                publishable-key="pk_test_51Pc6U3Roha3LTV1I0tFLbBcKvDLtk3eHrpCVbXzjaEAup7XHKCUoUpC17LUFF9STnOb5tF4WEO2K5PHSTzH5rJmw00KQSwpUhm"
-                >
-                </stripe-buy-button>
+                    <stripe-buy-button
+                    buy-button-id="buy_btn_1Pjpm2Roha3LTV1IHrYUvfuu"
+                    publishable-key="pk_test_51Pc6U3Roha3LTV1I0tFLbBcKvDLtk3eHrpCVbXzjaEAup7XHKCUoUpC17LUFF9STnOb5tF4WEO2K5PHSTzH5rJmw00KQSwpUhm"
+                    >
+                    </stripe-buy-button>
                 </Col>
             </Row>
             <section style={secondStyle}>
