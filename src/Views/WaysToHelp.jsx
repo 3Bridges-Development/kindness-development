80490b419cede308f7c4dac70e9c5add0af05d1c
nkleinmann <nekleinmann@gmail.com>
2024-08-04 17:55:33 -0400

    fix payment link

--- src/Views/WaysToHelp.jsx
+++ src/Views/WaysToHelp.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import newWave1 from "../Assets/newWave1.png";
 import { Row, Col } from "react-bootstrap";
-// import wave5 from "../Assets/wave5.png";
 
-// TODO: add top image when content is ready
-// const firstStyle = {
-//     backgroundImage:
-//         // `url('${newWave1Rotated}')`,
-//         `url('${wave5}')`,
-//         // `url('${newWaveYellow}')`,
-//     minHeight: "100vh",
-//     backgroundPosition: "center top",
-//     backgroundSize: "cover",
-//     backgroundRepeat: "no-repeat",
-// };
-
-const secondStyle = {
+const bottomStyle = {
     backgroundImage:
         `url('${newWave1}')`,
-    height: "100vh",
+    height: "50vh",
     backgroundPosition: "center bottom",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
@@ -35,10 +22,6 @@ function WaysToHelp() {
             </Row>
             <Row className="theWaysToHelp">
                 <Col md="12 justify-content-center">
-                    <script async
-                    src="https://js.stripe.com/v3/buy-button.js">
-                    </script>
-
                     <stripe-buy-button
                     buy-button-id="buy_btn_1Pjpm2Roha3LTV1IHrYUvfuu"
                     publishable-key="pk_test_51Pc6U3Roha3LTV1I0tFLbBcKvDLtk3eHrpCVbXzjaEAup7XHKCUoUpC17LUFF9STnOb5tF4WEO2K5PHSTzH5rJmw00KQSwpUhm"
@@ -46,7 +29,7 @@ function WaysToHelp() {
                     </stripe-buy-button>
                 </Col>
             </Row>
-            <section style={secondStyle}>
+            <section style={bottomStyle}>
             </section>
         </>
     )
