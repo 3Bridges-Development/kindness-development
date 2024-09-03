271d57703d25af8180e4c9475b4091851bd4cf83
nkleinmann <nekleinmann@gmail.com>
2024-09-02 20:52:27 -0400

    add book content

--- src/Components/Programs.jsx
+++ src/Components/Programs.jsx
@@ -26,6 +26,17 @@ function Programs() {
                             </Row>
                         </Col>
                     </Row>
+                    <Row className="justify-content-center">
+                        <Col className="textPlug" md="6">
+                            <Row width="40%">
+                                <p className="kindness">PLUG - Book for Purchase</p>
+                                <a href="https://www.amazon.com/PLUG-Parents-Ultimate-Children-Insanity-ebook/dp/B0DC5NDZRC/ref=sr_1_1?crid=6HTB2T6Q9KGN&dib=eyJ2IjoiMSJ9.9loPw1X9_TT-eyptG3eKUw.qEifSnHBzTiYSmc08-Ddm5nL3k76XwG59v751OmVEn0&dib_tag=se&keywords=Plug-Parents+Life%27s+Ultimate+Gifts&qid=1723296090&rnid=2941120011&s=books&sprefix=plug-parents+life%27s+ultimate+gifts%2Caps%2C171&sr=1-1" target="_blank" rel="noopener noreferrer" className="plugLink"> 
+                                    <p className="plugText">PLUG - Parents Life's Ultimate Gift: Steps to Raising Healthy Children In A World of Insanity</p>
+                                </a>
+                                <p className="plugText">by Delores Wallace</p>
+                            </Row>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
             <Row className="justify-content-center programs" id="thePrograms">
